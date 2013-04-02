265b1e2797d9825f40a340a7c177e8d358e7a533
Ian Escarro <ian.escarro@gmail.com>
2013-04-02 22:43:56 +0800

    moved some html helper functions to formhelper

--- HW.Core/Helpers/HtmlHelper.cs
+++ HW.Core/Helpers/HtmlHelper.cs
@@ -14,11 +14,6 @@ namespace HW.Core.Helpers
 {
 	public static class HtmlHelper
 	{
-		public static string Hidden(string id, string val)
-		{
-			return string.Format("<input type='hidden' id='{0}' value='{1}'/>", id, val);
-		}
-		
 		public static string Anchor(string text, string url)
 		{
 			return Anchor(text, url, new Dictionary<string, string>());
@@ -49,6 +44,14 @@ namespace HW.Core.Helpers
 			}
 		}
 		
+		public static string AnchorImage(string url, string image, bool random)
+		{
+			if (random) {
+				return string.Format("<a href='{0}&Rnd={2}'><img src='{1}'></a>", url, image, GetRandomInt());
+			}
+			return string.Format("<a href='{0}'><img src='{1}'></a>", url, image);
+		}
+		
 		public static string Image(string url)
 		{
 			return string.Format("<img src='{0}'/>", url);
@@ -59,24 +62,14 @@ namespace HW.Core.Helpers
 			return string.Format("<img src='{0}' width='{1}' height='{2}'/>", url, width, height);
 		}
 		
-		public static string Input(string name, string value, string clas, string placeholder)
-		{
-			return string.Format("<input type='text' name='{0}' id='{0}' value='{1}' class='{2}' placeholder='{3}'/>", name, value, clas, placeholder);
-		}
-		
-		public static string Password(string name, string value, string clas, string placeholder)
-		{
-			return string.Format("<input type='password' name='{0}' id='{0}' value='{1}' class='{2}' placeholder='{3}'/>", name, value, clas, placeholder);
-		}
-		
 		public static string DropDownList<T>(IList<T> lists)
 		{
 			StringBuilder s = new StringBuilder();
-			s.Append("<ul>");
+			s.AppendLine("<ul>");
 			foreach (var l in lists) {
-				s.Append(string.Format("<li>{0}</li>", l));
+				s.AppendLine(string.Format("<li>{0}</li>", l));
 			}
-			s.Append("</ul>");
+			s.AppendLine("</ul>");
 			return s.ToString();
 		}
 		
@@ -118,6 +111,24 @@ namespace HW.Core.Helpers
 		public static string CreatePage(P p)
 		{
 			return p.ToString();
+		}
+	}
+	
+	public class FormHelper
+	{
+		public static string Input(string name, string value, string clas, string placeholder)
+		{
+			return string.Format("<input type='text' name='{0}' id='{0}' value='{1}' class='{2}' placeholder='{3}'/>", name, value, clas, placeholder);
+		}
+		
+		public static string Password(string name, string value, string clas, string placeholder)
+		{
+			return string.Format("<input type='password' name='{0}' id='{0}' value='{1}' class='{2}' placeholder='{3}'/>", name, value, clas, placeholder);
+		}
+		
+		public static string Hidden(string id, string val)
+		{
+			return string.Format("<input type='hidden' id='{0}' value='{1}'/>", id, val);
 		}
 	}
 	
