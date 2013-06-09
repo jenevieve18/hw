c9d2d7361c081df98e09ea9861c85b8df88adb0f
Ian Escarro <ian.escarro@gmail.com>
2013-06-09 21:43:58 +0800

    updated tests

--- src/HW.Tests/Helpers/PageTests.cs
+++ src/HW.Tests/Helpers/PageTests.cs
@@ -4,29 +4,23 @@
 //	</file>
 
 using System;
-using System.Collections.Generic;
-using System.Text;
-using DocumentFormat.OpenXml;
-using DocumentFormat.OpenXml.Packaging;
-using DocumentFormat.OpenXml.Wordprocessing;
 using HW.Core.Helpers;
 using NUnit.Framework;
-using w = DocumentFormat.OpenXml.Wordprocessing;
 
-namespace HW.Tests
+namespace HW.Tests.Helpers
 {
 	[TestFixture]
-	public class Test2
+	public class PageTests
 	{
 		[Test]
-		public void TestMethod()
+		public void TestQueryString()
 		{
 			Q q = new Q();
 			q.Add("LangID", 1);
 			q.Add("PLOT", "BOXPLOT");
 			q.Add("PRUID", 1);
 			q.Add("TEST", "");
-			Console.WriteLine(q.ToString());
+			Assert.AreEqual("LangID=1&PLOT=BOXPLOT&PRUID=1&TEST=", q.ToString());
 		}
 	}
 }
--- src/HW.Tests/Test4.cs
+++ src/HW.Tests/Test4.cs
@@ -4,7 +4,9 @@
 //	</file>
 
 using System;
+using HW.Core.Models;
 using HW.Core.Repositories.NHibernate;
+using HW.Core.Repositories.Sql;
 using NUnit.Framework;
 
 namespace HW.Tests
@@ -31,6 +33,27 @@ namespace HW.Tests
 		[Test]
 		public void b()
 		{
+		}
+		
+		[Test]
+		public void c()
+		{
+			string s = "\r\nSELECT d.Department,\r\n\tdbo.cf_departmentDepth(d.DepartmentID),\r\n\td.DepartmentID,\r\n\t(\r\n\t\tSELECT COUNT(*) FROM Department x\r\n\t\tWHERE (x.ParentDepartmentID = d.ParentDepartmentID OR x.ParentDepartmentID IS NULL AND d.ParentDepartmentID IS NULL)\r\n\t\tAND d.SponsorID = x.SponsorID\r\n\t\tAND d.SortString < x.SortString\r\n\t),\r\n\tISNULL(sad.DepartmentID, sa.SuperUser),\r\n\td.DepartmentShort\r\nFROM Department d\r\nINNER JOIN SponsorAdmin sa ON sa.SponsorAdminID = 514\r\nLEFT OUTER JOIN SponsorAdminDepartment sad ON d.DepartmentID = sad.DepartmentID\r\nAND sad.SponsorAdminID = 514 \r\nWHERE d.SponsorID = 83\r\nORDER BY d.SortString";
+			Console.WriteLine(s);
+		}
+		
+		[Test]
+		public void d()
+		{
+			new SqlSponsorRepository().UpdateSponsorAdmin(
+				new SponsorAdmin {
+					Name = "Karin",
+					Email = "",
+					Usr = "",
+					Password = "",
+					Sponsor = new Sponsor { Id = 1 }
+				}
+			);
 		}
 	}
 }
