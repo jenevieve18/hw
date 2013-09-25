7aafdab70c441832b4d457cc4faceb9d9378d63d
iescarro <ian.escarro@gmail.com>
2013-09-25 22:40:33 +0800

    removed name in helper files

--- src/HW.Core/Helpers/Db2.cs
+++ src/HW.Core/Helpers/Db2.cs
@@ -1,9 +1,4 @@
-﻿//	<file>
-//		<license></license>
-//		<owner name="Ian Escarro" email="ian.escarro@gmail.com"/>
-//	</file>
-
-using System;
+﻿using System;
 using System.Configuration;
 using System.Data;
 using System.Data.SqlClient;
