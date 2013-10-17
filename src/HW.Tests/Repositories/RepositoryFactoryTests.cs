1aa9a2940e9161684d2fb79f411107dd1c5ba2d7
iescarro <ian.escarro@gmail.com>
2013-10-17 10:56:00 +0800

    clean up

--- src/HW.Tests/Repositories/RepositoryFactoryTests.cs
+++ src/HW.Tests/Repositories/RepositoryFactoryTests.cs
@@ -16,22 +16,5 @@ namespace HW.Tests.Repositories
 		{
 			f = new SqlRepositoryFactory();
 		}
-		
-		[Test]
-		public void TestValues()
-		{
-			Assert.IsInstanceOfType(typeof(IAnswerRepository), f.CreateAnswerRepository());
-			Assert.IsInstanceOfType(typeof(IDepartmentRepository), f.CreateDepartmentRepository());
-			Assert.IsInstanceOfType(typeof(IExerciseRepository), f.CreateExerciseRepository());
-			Assert.IsInstanceOfType(typeof(IIndexRepository), f.CreateIndexRepository());
-			Assert.IsInstanceOfType(typeof(ILanguageRepository), f.CreateLanguageRepository());
-			Assert.IsInstanceOfType(typeof(IManagerFunctionRepository), f.CreateManagerFunctionRepository());
-			Assert.IsInstanceOfType(typeof(IOptionRepository), f.CreateOptionRepository());
-			Assert.IsInstanceOfType(typeof(IProjectRepository), f.CreateProjectRepository());
-			Assert.IsInstanceOfType(typeof(IQuestionRepository), f.CreateQuestionRepository());
-			Assert.IsInstanceOfType(typeof(IReportRepository), f.CreateReportRepository());
-			Assert.IsInstanceOfType(typeof(ISponsorRepository), f.CreateSponsorRepository());
-			Assert.IsInstanceOfType(typeof(IUserRepository), f.CreateUserRepository());
-		}
 	}
 }
