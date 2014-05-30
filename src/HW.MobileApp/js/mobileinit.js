d20529b48aa9362738d997ad42e121e96326b205
iescarro <ian.escarro@gmail.com>
2014-05-30 21:54:36 +0800

    removed css and js. reference now is from jquery mobile CDN

--- src/HW.MobileApp/js/mobileinit.js
+++
@@ -1,9 +0,0 @@
-// configure jquery mobile
-$(document).on("mobileinit", function () {
-    // turn off transition
-    $.mobile.defaultPageTransition = 'none';
-    $("body .button_right").buttonMarkup({ iconpos: "right" });
-    $('a').buttonMarkup({ corners: false , theme: "b"});
-    $.mobile.changePage.defaults.allowSamePageTransitions = true;
-    $.mobile.page.prototype.options.domCache = true;
-});
