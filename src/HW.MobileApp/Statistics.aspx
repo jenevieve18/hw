5db676e3458dba9ad9757a19f73812f332c56955
iescarro <ian.escarro@gmail.com>
2014-06-02 10:14:53 +0800

    added statistics page

---
+++ src/HW.MobileApp/Statistics.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MobileApp.Master" AutoEventWireup="true" CodeBehind="Statistics.aspx.cs" Inherits="HW.MobileApp.Statistics" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+
+<div data-role="header" data-theme="b" data-position="fixed">
+    <a href="Dashboard.aspx" data-icon="arrow-l">My Health</a>
+    <h1>Statistics</h1>
+    <a href="#" data-icon="check">View</a>
+</div>
+<div data-role="content">
+    <ul data-role="listview">
+        <% foreach (var q in questions) { %>
+            <li>
+                <%= q.QuestionText %>
+            </li>
+        <% } %>
+    </ul>
+</div>
+
+</asp:Content>
---
+++ src/HW.MobileApp/Statistics.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using HW.Core.Helpers;
+
+namespace HW.MobileApp
+{
+    public partial class Statistics : System.Web.UI.Page
+    {
+        HWService.ServiceSoap service = new HWService.ServiceSoapClient();
+        protected List<HWService.Question> questions = new List<HWService.Question>();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            HtmlHelper.RedirectIf(Session["token"] == null, "Login.aspx");
+
+            string token = Session["token"] == null ? "" : Session["token"].ToString();
+            var forms = service.FormEnum(new HWService.FormEnumRequest(token, 1, 10)).FormEnumResult;
+            foreach (var f in forms)
+            {
+                var q = service.FormQuestionEnum(new HWService.FormQuestionEnumRequest(token, 1, f.formKey, 10)).FormQuestionEnumResult;
+                questions.AddRange(q);
+            }
+        }
+    }
+}
