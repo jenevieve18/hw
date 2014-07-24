dd0a0e893172b07a6f108ec65304b07e7454062d
Ian Escarro <ian.escarro@gmail.com>
2014-07-24 10:45:04 +0800

    updated logging service including passed parameter to query

--- src/HW.Grp/Org.aspx.cs
+++ src/HW.Grp/Org.aspx.cs
@@ -2154,9 +2154,13 @@ WHERE u.UserID = {0}",
 									rs.GetInt32(1)
 								);
                                 SqlDataReader rs2 = Db.rs(query);
-                                LoggingService.Info(query);
+                                //LoggingService.Info(query);
 								while (rs2.Read()) {
 									#region Create new profile
+                                    LoggingService.Info("rs.GetInt32(1): " + rs.GetInt32(1));
+                                    LoggingService.Info("sponsorID: " + sponsorID);
+                                    LoggingService.Info("DepartmentID.SelectedValue: " + DepartmentID.SelectedValue);
+                                    LoggingService.Info("rs2.GetInt32(1): " + rs2.GetInt32(1));
 									query = string.Format(
 										@"
 INSERT INTO UserProfile (UserID,SponsorID,DepartmentID,ProfileComparisonID,Created)
