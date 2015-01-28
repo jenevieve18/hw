9e782bedd6212220f15067fe11807e479dd1dd67
Ian Escarro <ian.escarro@gmail.com>
2015-01-28 15:49:37 +0800

    clean up and fix for GetLegend used by exercise page in grp

--- src/HW.Core/Models/Language.cs
+++ src/HW.Core/Models/Language.cs
@@ -21,44 +21,44 @@ namespace HW.Core.Models
 			return lid == 1 ? "medelvärde" : "mean value";
 		}
 		
-		public static string GetGroupExercise(int lid)
-		{
-			switch (lid) {
-					case 0: return "Grupp-<br/>övningar";
-					case 1: return "Group-<br/>exercises";
-					default: throw new NotSupportedException();
-			}
-		}
+//		public static string GetGroupExercise(int lid)
+//		{
+//			switch (lid) {
+//					case 0: return "Grupp-<br/>övningar";
+//					case 1: return "Group-<br/>exercises";
+//					default: throw new NotSupportedException();
+//			}
+//		}
 		
-		public static string GetChooseArea(int lid)
-		{
-			// FIXME: Why 0 on Swedish? See setculture where Swedish is 1.
-			switch (lid) {
-					case 0: return "Välj område";
-					case 1: return "Choose area";
-					default: throw new NotSupportedException();
-			}
-		}
+//		public static string GetChooseArea(int lid)
+//		{
+//			// FIXME: Why 0 on Swedish? See setculture where Swedish is 1.
+//			switch (lid) {
+//					case 0: return "Välj område";
+//					case 1: return "Choose area";
+//					default: throw new NotSupportedException();
+//			}
+//		}
 		
-		public static string GetChooseCategory(int lid)
-		{
-			// FIXME: Why 0 on Swedish? See setculture where Swedish is 1.
-			switch (lid) {
-					case 0: return "Välj kategori";
-					case 1: return "Choose category";
-					default: throw new NotSupportedException();
-			}
-		}
+//		public static string GetChooseCategory(int lid)
+//		{
+//			// FIXME: Why 0 on Swedish? See setculture where Swedish is 1.
+//			switch (lid) {
+//					case 0: return "Välj kategori";
+//					case 1: return "Choose category";
+//					default: throw new NotSupportedException();
+//			}
+//		}
 		
-		public static string GetSortingOrder(int lid, int bx)
-		{
-			// FIXME: Why 0 on Swedish? See setculture where Swedish is 1.
-			switch (lid) {
-					case 0: return bx + " övningar - Sortering:";
-					case 1: return bx + " exercises - Order:";
-					default: throw new NotSupportedException();
-			}
-		}
+//		public static string GetSortingOrder(int lid, int bx)
+//		{
+//			// FIXME: Why 0 on Swedish? See setculture where Swedish is 1.
+//			switch (lid) {
+//					case 0: return bx + " övningar - Sortering:";
+//					case 1: return bx + " exercises - Order:";
+//					default: throw new NotSupportedException();
+//			}
+//		}
 		
 		public static string GetLegend(int lid)
 		{
@@ -79,14 +79,14 @@ namespace HW.Core.Models
 			}
 		}
 
-		public static void SetCurrentCulture2(int lid)
-		{
-			switch (lid)
-			{
-				case 0: Thread.CurrentThread.CurrentCulture = new CultureInfo("sv-SE"); break;
-				case 1: Thread.CurrentThread.CurrentCulture = new CultureInfo("en-US"); break;
-				default: throw new NotSupportedException();
-			}
-		}
+//		public static void SetCurrentCulture2(int lid)
+//		{
+//			switch (lid)
+//			{
+//				case 0: Thread.CurrentThread.CurrentCulture = new CultureInfo("sv-SE"); break;
+//				case 1: Thread.CurrentThread.CurrentCulture = new CultureInfo("en-US"); break;
+//				default: throw new NotSupportedException();
+//			}
+//		}
 	}
 }
