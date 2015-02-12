2d0c06a81c18aea2d1a06908cc6badd753582480
iescarro <ian.escarro@gmail.com>
2015-02-12 09:37:56 +0800

    * allow sql drop for now in sql utility (with caution)

--- src/HW.Grp/sql.aspx.cs
+++ src/HW.Grp/sql.aspx.cs
@@ -18,30 +18,21 @@ namespace HW.Grp
 
         protected void Button1_Click(object sender, EventArgs e)
         {
-            if ((TextBox1.Text.ToUpper().Contains("DELETE") && !TextBox1.Text.ToUpper().Contains("WHERE")) || (TextBox1.Text.ToUpper().Contains("UPDATE") && !TextBox1.Text.ToUpper().Contains("WHERE")))
-            {
+            if ((TextBox1.Text.ToUpper().Contains("DELETE") && !TextBox1.Text.ToUpper().Contains("WHERE")) || (TextBox1.Text.ToUpper().Contains("UPDATE") && !TextBox1.Text.ToUpper().Contains("WHERE"))) {
                 Label1.Text = "Please provide where clause for delete statement.";
-            }
-            else if (TextBox1.Text.ToUpper().Contains("DROP"))
-            {
-                Label1.Text = "Please don't use drop statement in this utility page.";
-            }
-            else
-            {
-                try
-                {
+//        	} else if (TextBox1.Text.ToUpper().Contains("DROP")) {
+//        		Label1.Text = "Please don't use drop statement in this utility page.";
+            } else {
+                try {
                     SqlConnection con = new SqlConnection(ConfigurationManager.AppSettings["healthWatchSqlConnection"]);
                     SqlDataAdapter da = new SqlDataAdapter(TextBox1.Text, con);
                     DataSet ds = new DataSet();
                     da.Fill(ds);
-                    if (ds.Tables.Count > 0)
-                    {
+                    if (ds.Tables.Count > 0) {
                         GridView1.DataSource = ds;
                         GridView1.DataBind();
                     }
-                }
-                catch (Exception ex)
-                {
+                } catch (Exception ex) {
                     Label1.Text = ex.Message;
                 }
             }
