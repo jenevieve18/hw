ffc1366e43a94b4678378ceeee60198aabe251c3
iescarro <ian.escarro@gmail.com>
2015-06-01 13:50:26 +0800

    added customer and invoice repository tests.

---
+++ src/HW.Tests/Invoicing/Repositories/CustomerRepositoryTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using HW.Invoicing.Core.Repositories.Sql;
+using NUnit.Framework;
+
+namespace HW.Tests.Invoicing.Repositories
+{
+	[TestFixture]
+	public class CustomerRepositoryTests
+	{
+		SqlCustomerRepository r;
+		
+		[SetUp]
+		public void Setup()
+		{
+			r = new SqlCustomerRepository();
+		}
+		
+		[Test]
+		public void TestFindAll()
+		{
+			foreach (var c in r.FindAll()) {
+				Console.WriteLine(c.Name);
+			}
+		}
+	}
+}
---
+++ src/HW.Tests/Invoicing/Repositories/InvoiceRepositoryTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using HW.Invoicing.Core.Repositories.Sql;
+using NUnit.Framework;
+using System.Linq;
+
+namespace HW.Tests.Invoicing.Repositories
+{
+	[TestFixture]
+	public class InvoiceRepositoryTests
+	{
+		SqlInvoiceRepository r;
+		
+		[SetUp]
+		public void Setup()
+		{
+			r = new SqlInvoiceRepository();
+		}
+		
+		[Test]
+		public void TestFindAll()
+		{
+			foreach (var i in r.FindAll()) {
+				Console.WriteLine(i.Date);
+			}
+		}
+	}
+}
