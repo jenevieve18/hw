7493e40a77d45b6255dc0d19ce968e7106735961
iescarro <ian.escarro@gmail.com>
2015-07-08 14:29:46 +0800

    updated invoice binary files.

---
+++ src/HW.Tests/Helpers/InvoiceExporterTests.cs
@@ -0,0 +1,23 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Ian
+ * Date: 7/8/2015
+ * Time: 2:05 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using NUnit.Framework;
+
+namespace HW.Tests.Helpers
+{
+	[TestFixture]
+	public class InvoiceExporterTests
+	{
+		[Test]
+		public void TestMethod()
+		{
+			// TODO: Add your test.
+		}
+	}
+}
