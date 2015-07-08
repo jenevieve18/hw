462f7db5c867b7fe8cb980decdd40725c99082c0
iescarro <ian.escarro@gmail.com>
2015-07-08 14:58:17 +0800

    updated invoice binary files.

--- src/HW.Invoicing.Core/Models/Invoice.cs
+++ src/HW.Invoicing.Core/Models/Invoice.cs
@@ -90,59 +90,58 @@ namespace HW.Invoicing.Core.Models
 		{
 			MemoryStream output = new MemoryStream();
 
-			using (var existingFileStream = new FileStream(existingFileName, FileMode.Open)) {
-				using (var reader = new PdfReader(existingFileStream)) {
-					var stamper = new PdfStamper(reader, output) {
-						FormFlattening = true,
-						FreeTextFlattening = true
-					};
-					var form = stamper.AcroFields;
-					var fieldKeys = form.Fields.Keys;
-					
-					form.SetField("Text1", invoice.Customer.Number);
-					form.SetField("Text2", invoice.Number);
-					form.SetField("Text3", invoice.Date.Value.ToString("yyyy-MM-dd"));
-					form.SetField("Text4", invoice.MaturityDate.Value.ToString("yyyy-MM-dd"));
-					
-					form.SetField("Text5", invoice.Customer.YourReferencePerson);
-					form.SetField("Text6", invoice.Customer.OurReferencePerson);
-					
-					form.SetField("Text6B", invoice.Customer.ToString());
-					
-					form.SetField("Text10b", invoice.SubTotal.ToString());
-					form.SetField("Text13", invoice.TotalAmount.ToString());
-					
-					string items = "";
-					string quantities = "";
-					string units = "";
-					string prices = "";
-					string amounts = "";
-					foreach (var t in invoice.Timebooks) {
-						items += t.ToString() + "\n\n";
-						quantities += t.Timebook.Quantity.ToString() + "\n\n";
-						units += t.Timebook.Item.Unit.Name + "\n\n";
-						prices += t.Timebook.Price.ToString() + "\n\n";
-						amounts += t.Timebook.Amount.ToString() + "\n\n";
-					}
-					form.SetField("Text7", items);
-					form.SetField("Text8", quantities);
-					form.SetField("Text9", units);
-					form.SetField("Text9b", prices);
-					form.SetField("Text9c", amounts);
-
-					if (invoice.VATs.ContainsKey(25))
-					{
-						form.SetField("Text11b", 25.ToString());
-						form.SetField("Text12", invoice.VATs[25].ToString());
-					}
-
-					foreach (var s in form.Fields.Keys)
-					{
-						stamper.PartialFormFlattening(s);
-					}
-					stamper.Close();
-				}
+			var existingFileStream = new FileStream(existingFileName, FileMode.Open);
+			var reader = new PdfReader(existingFileStream);
+			var stamper = new PdfStamper(reader, output) {
+//				FormFlattening = true,
+				FreeTextFlattening = true
+			};
+			var form = stamper.AcroFields;
+			var fieldKeys = form.Fields.Keys;
+			
+			form.SetField("Text1", invoice.Customer.Number);
+			form.SetField("Text2", invoice.Number);
+			form.SetField("Text3", invoice.Date.Value.ToString("yyyy-MM-dd"));
+			form.SetField("Text4", invoice.MaturityDate.Value.ToString("yyyy-MM-dd"));
+			
+			form.SetField("Text5", invoice.Customer.YourReferencePerson);
+			form.SetField("Text6", invoice.Customer.OurReferencePerson);
+			
+			form.SetField("Text6B", invoice.Customer.ToString());
+			
+			form.SetField("Text10b", invoice.SubTotal.ToString());
+			form.SetField("Text13", invoice.TotalAmount.ToString());
+			
+			string items = "";
+			string quantities = "";
+			string units = "";
+			string prices = "";
+			string amounts = "";
+			foreach (var t in invoice.Timebooks) {
+				items += t.ToString() + "\n\n";
+				quantities += t.Timebook.Quantity.ToString() + "\n\n";
+				units += t.Timebook.Item.Unit.Name + "\n\n";
+				prices += t.Timebook.Price.ToString() + "\n\n";
+				amounts += t.Timebook.Amount.ToString() + "\n\n";
 			}
+			form.SetField("Text7", items);
+			form.SetField("Text8", quantities);
+			form.SetField("Text9", units);
+			form.SetField("Text9b", prices);
+			form.SetField("Text9c", amounts);
+
+			if (invoice.VATs.ContainsKey(25))
+			{
+				form.SetField("Text11b", 25.ToString());
+				form.SetField("Text12", invoice.VATs[25].ToString());
+			}
+
+			foreach (var s in form.Fields.Keys)
+			{
+				stamper.PartialFormFlattening(s);
+			}
+//			reader.Close();
+//			stamper.Close();
 
 			return output;
 		}
