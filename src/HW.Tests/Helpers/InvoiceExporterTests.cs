66272906b2464b0aed4a0ccb93070b61671c6a5a
iescarro <ian.escarro@gmail.com>
2015-07-09 14:16:45 +0800

    updated invoice exporter tests.

--- src/HW.Tests/Helpers/InvoiceExporterTests.cs
+++ src/HW.Tests/Helpers/InvoiceExporterTests.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Diagnostics;
 using System.IO;
+using HW.Invoicing.Core.Helpers;
 using HW.Invoicing.Core.Models;
 using HW.Invoicing.Core.Repositories.Sql;
 using iTextSharp.text.pdf;
@@ -24,15 +26,17 @@ namespace HW.Tests.Helpers
 		public void TestExport()
 		{
 			using (FileStream f = new FileStream(@"test.pdf", FileMode.Create, FileAccess.Write)) {
-				MemoryStream s = e.Export(i, @"IHG faktura MALL Ian without comments.pdf");
+				MemoryStream s = e.Export(i, @"IHG faktura MALL Ian without comments.pdf", @"calibri.ttf");
 				s.WriteTo(f);
 			}
+			Process.Start("test.pdf");
 		}
 		
 		[Test]
 		public void TestExport2()
 		{
 			e.Export2(i, @"IHG faktura MALL Ian without comments.pdf");
+			Process.Start("test.pdf");
 		}
 	}
 }
