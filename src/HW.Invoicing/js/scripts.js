f24bad71a079f099ecdc6c7c056705fe093d0bac
iescarro <ian.escarro@gmail.com>
2015-07-09 17:45:07 +0800

    month calculation for the date done.

--- src/HW.Invoicing/js/scripts.js
+++ src/HW.Invoicing/js/scripts.js
@@ -1,12 +1,48 @@
 
-function monthDiff(d1, d2) {
+/*function monthDiff(d1, d2) {
     var months;
     months = (d2.getFullYear() - d1.getFullYear()) * 12;
-    months -= d1.getMonth(); // + 1;
+    months -= d1.getMonth();
     months += d2.getMonth();
     return months <= 0 ? 0 : months;
+}*/
+
+function getDaysInAMonth(month, year) {
+    var isLeapYear = year % 4 == 0 && (year % 100 != 0 && year % 400 == 0);
+    var febDays = isLeapYear ? 29 : 28;
+    var days = [
+        31,
+        febDays,
+        31,
+        30,
+        31,
+        30,
+        31,
+        31,
+        30,
+        31,
+        30,
+        31
+    ];
+    var d = days[month];
+    return d;
 }
 
+function monthDiff(d1, d2) {
+    var months = (d2.getFullYear() - d1.getFullYear()) * 12;
+    months -= d1.getMonth();
+    months += d2.getMonth();
+
+    var daysOfMonth1 = getDaysInAMonth(d1.getMonth(), d1.getFullYear());
+    var days = daysOfMonth1 - d1.getDate();
+    days += d2.getDate();
+    days -= daysOfMonth1;
+    var extraMonth = parseFloat(days) / daysOfMonth1;
+    extraMonth = Math.ceil(extraMonth * 2) / 2;
+
+    months += extraMonth;
+    return months;
+}
 
 Object.size = function (obj) {
     var size = 0, key;
