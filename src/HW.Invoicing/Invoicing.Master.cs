cc63074f70946f36eeb5a52fa84d8f8f9afc4feb
iescarro <ian.escarro@gmail.com>
2015-07-16 14:06:22 +0800

    commented invoicing master page for now to be able to add company columns.

--- src/HW.Invoicing/Invoicing.Master.cs
+++ src/HW.Invoicing/Invoicing.Master.cs
@@ -18,7 +18,7 @@ namespace HW.Invoicing
         protected void Page_Load(object sender, EventArgs e)
         {
             int companyId = ConvertHelper.ToInt32(Session["CompanyId"]);
-            company = r.Read(companyId);
+            //company = r.Read(companyId);
         }
     }
 }
