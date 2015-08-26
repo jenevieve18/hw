4334bd27c50b1ad78f80976674fd28bd8df86c75
iescarro <ian.escarro@gmail.com>
2015-08-26 15:04:49 +0800

    updated test case

---
+++ src/HW.Tests/Helpers/AgreementExporterTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using HW.Invoicing.Core.Helpers;
+using HW.Invoicing.Core.Models;
+using HW.Invoicing.Core.Repositories.Sql;
+using NUnit.Framework;
+
+namespace HW.Tests.Helpers
+{
+	[TestFixture]
+	public class AgreementExporterTests
+	{
+		HCGEAgreementExporter hcg;
+		SqlCustomerRepository r = new SqlCustomerRepository();
+		CustomerAgreement a;
+		
+		[SetUp]
+		public void Setup()
+		{
+			hcg = new HCGEAgreementExporter();
+			a = r.ReadAgreement(1);
+			a.Customer = r.Read(a.Customer.Id);
+		}
+		
+		[Test]
+		public void TestExport()
+		{
+			using (FileStream f = new FileStream(@"test.pdf", FileMode.Create, FileAccess.Write)) {
+				MemoryStream s = hcg.Export(a, @"HCG Avtal HCGE-PDF form example without comments.pdf", "calibri.ttf");
+				s.WriteTo(f);
+			}
+			Process.Start("test.pdf");
+		}
+	}
+}
--- src/HW.Tests/Helpers/InvoiceExporterTests.cs
+++ src/HW.Tests/Helpers/InvoiceExporterTests.cs
@@ -13,41 +13,50 @@ namespace HW.Tests.Helpers
 	public class InvoiceExporterTests
 	{
 		Invoice i;
-		IHGFInvoiceExporter e;
+		IHGFInvoiceExporter ihg;
+		HCGFInvoiceExporter hcg;
 		
 		[SetUp]
 		public void Setup()
 		{
 			i = new SqlInvoiceRepository().Read(52);
-			e = new IHGFInvoiceExporter();
+			
+			ihg = new IHGFInvoiceExporter();
+			hcg = new HCGFInvoiceExporter();
 		}
 		
 		[Test]
-		public void TestExport()
+		public void TestIHGFExporter()
 		{
 			using (FileStream f = new FileStream(@"test.pdf", FileMode.Create, FileAccess.Write)) {
-				MemoryStream s = e.Export(i, @"IHG faktura MALL Ian without comments.pdf", @"calibri.ttf");
+				MemoryStream s = ihg.Export(i, @"IHG faktura MALL Ian without comments.pdf", "calibri.ttf");
 				s.WriteTo(f);
 			}
 			Process.Start("test.pdf");
 		}
 		
-		[Test]
-		public void TestExport2()
-		{
-			e.Export2(i, @"IHG faktura MALL Ian without comments.pdf");
-			Process.Start("test.pdf");
-		}
+//		[Test]
+//		public void TestIHGFExporter2()
+//		{
+//			ihg.Export2(i, @"IHG faktura MALL Ian without comments.pdf");
+//			Process.Start("test.pdf");
+//		}
 		
 		[Test]
-		public void a()
+		public void TestHCGFExporter()
 		{
-			var x = new HCGFInvoiceExporter();
 			using (FileStream f = new FileStream(@"test.pdf", FileMode.Create, FileAccess.Write)) {
-				MemoryStream s = x.Export(i, @"HCG Fakturamall tom without comments.pdf", @"calibri.ttf");
+				MemoryStream s = hcg.Export(i, @"HCG Fakturamall tom without comments.pdf", "calibri.ttf");
 				s.WriteTo(f);
 			}
 			Process.Start("test.pdf");
 		}
+		
+//		[Test]
+//		public void TestHCGFExporter2()
+//		{
+//			hcg.Export2(i, @"HCG Fakturamall tom without comments.pdf", "calibri.ttf");
+//			Process.Start("test.pdf");
+//		}
 	}
 }
