9197e75b51437cc90c38286db7b3582ab681b466
iescarro <ian.escarro@gmail.com>
2015-10-09 21:30:19 +0800

    adding answers and total columns

--- src/HW.Grp/doc/grp_2.10.sql
+++ src/HW.Grp/doc/grp_2.10.sql
@@ -3,3 +3,6 @@ use healthWatch;
 alter table SponsorAdmin add UniqueKeyUsed int;
 
 alter table SponsorExtendedSurvey add Answers int;
+
+alter table SponsorExtendedSurveyDepartment add Total int;
+alter table SponsorExtendedSurveyDepartment add Answers int;
