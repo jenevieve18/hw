7d055aca3d037fb410a1a29b37507e788b9be716
iescarro <ian.escarro@gmail.com>
2015-11-11 20:56:16 +0800

    updated sql schema

--- src/HW.Invoicing/doc/schema_1.2.sql
+++ src/HW.Invoicing/doc/schema_1.2.sql
@@ -1,3 +1,13 @@
 use invoicing;
 
 alter table CustomerTimebook add DateHidden int;
+
+create table Milestone(
+	Id integer not null primary key identity,
+	Name varchar(255)
+);
+
+alter table Issue add Priority integer;
+
+alter table CustomerTimebook alter column Price decimal(16,2);
+alter table CustomerItem alter column Price decimal(16,2);
--- src/HW.Invoicing/doc/schema_1.3.sql
+++ src/HW.Invoicing/doc/schema_1.3.sql
@@ -1,11 +1,1 @@
 use invoicing;
-
-create table Milestone(
-	Id integer not null primary key identity,
-	Name varchar(255)
-);
-
-alter table Issue add Priority integer;
-
-alter table CustomerTimebook alter column Price decimal(16,2);
-alter table CustomerItem alter column Price decimal(16,2);
