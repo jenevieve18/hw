683a81009a947f22ec438e415d42d2b4f43052b5
iescarro <ian.escarro@gmail.com>
2016-01-05 05:54:20 +0800

    initial invoice send work flow

--- src/HW.Invoicing/InvoiceSend.aspx.cs
+++ src/HW.Invoicing/InvoiceSend.aspx.cs
@@ -6,6 +6,7 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using HW.Core.Helpers;
+using HW.Invoicing.Core.Helpers;
 using HW.Invoicing.Core.Repositories.Sql;
 
 namespace HW.Invoicing
@@ -17,8 +18,11 @@ namespace HW.Invoicing
 		
 		protected void Page_Load(object sender, EventArgs e)
 		{
+            HtmlHelper.RedirectIf(Session["UserId"] == null, string.Format("login.aspx?r={0}", HttpUtility.UrlEncode(Request.Url.PathAndQuery)));
+
 			int id = ConvertHelper.ToInt32(Request.QueryString["Id"]);
 			var invoice = ir.Read(id);
+            HtmlHelper.RedirectIf(invoice == null, "invoices.aspx");
 			
 			int companyId = ConvertHelper.ToInt32(Session["CompanyId"]);
 			var company = cr.Read(companyId);
@@ -31,18 +35,19 @@ namespace HW.Invoicing
 			}
 			
 			try {
-				string file = string.Format(Server.MapPath("~/uploads/{0} {1} {2}.pdf"), agreement.Id, agreement.Customer.Name, DateTime.Now.ToString("MMM yyyy"));
+				string file = string.Format(Server.MapPath("~/uploads/{0} {1} {2}.pdf"), invoice.Id, invoice.Customer.Name, DateTime.Now.ToString("MMM yyyy"));
 				string templateFileName = company.HasAgreementTemplate ? string.Format(Server.MapPath("~/uploads/{0}"), company.AgreementTemplate) : Server.MapPath(@"HCG Avtalsmall Latest.pdf");
 
 				using (FileStream fStream = new FileStream(file, FileMode.Create, FileAccess.Write)) {
 					var exporter = InvoiceExporterFactory.GetExporter2(company.InvoiceExporter);
-					var exported = exporter.Export(invoice, templateFileName, Server.MapPath(@"arial.ttf"), flatten);
-					mStream.WriteTo(fStream);
+					var exported = exporter.Export(invoice, templateFileName, Server.MapPath(@"arial.ttf"), false);
+					exported.WriteTo(fStream);
 				}
 				
+				string body = "";
 				MailHelper.SendMail(
 					"info@danhasson.se",
-					agreement.Email,
+					"test@localhost",
 					company.AgreementSignedEmailSubject,
 					body,
 					file
