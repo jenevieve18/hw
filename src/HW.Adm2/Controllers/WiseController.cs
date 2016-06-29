2c7031fbc8470029c09a0cf57749f4ef9ba7bd93
iescarro <ian.escarro@gmail.com>
2016-06-29 12:34:49 +0800

    updated the package for adm 2

---
+++ src/HW.Adm2/Controllers/WiseController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HW.Adm2.Controllers
+{
+    public class WiseController : Controller
+    {
+        //
+        // GET: /Wise/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ src/HW.Adm2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HW.Adm2.MvcApplication" Language="C#" %>
