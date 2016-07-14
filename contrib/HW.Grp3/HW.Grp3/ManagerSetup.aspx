02bf8a04346c3e9b5d2d4fd3bd9e659b1e7f55ff
iescarro <ian.escarro@gmail.com>
2016-07-14 17:04:19 +0800

    init grp3 asp.net mockup

--- contrib/HW.Grp3/HW.Grp3/ManagerSetup.aspx
+++
@@ -1,5 +0,0 @@
-﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Grp.Master" AutoEventWireup="true" CodeBehind="ManagerSetup.aspx.cs" Inherits="HW.Grp3.ManagerSetup" %>
-<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
-</asp:Content>
-<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
-</asp:Content>
---
+++ src/HW.Core/Libraries/ConnectionSingleton.cs
@@ -0,0 +1,36 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Ian
+ * Date: 7/14/2016
+ * Time: 2:50 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Data.SqlClient;
+
+namespace HW.Core.Libraries
+{
+	public class ConnectionSingleton
+	{
+		static SqlConnection instance;
+		
+		public static SqlConnection Instance {
+			get {
+				if (instance == null) {
+					instance = new SqlConnection();
+				}
+				return instance;
+			}
+		}
+		
+		ConnectionSingleton()
+		{
+		}
+		
+		public static void SetConnectionString(string connectionString)
+		{
+			Instance.ConnectionString = connectionString;
+		}
+	}
+}
