98884e5c05f3ace0c555d5f3d2ed0f1e1a4d7aba
iescarro <ian.escarro@gmail.com>
2016-08-12 22:02:09 +0800

    updated eform

--- src/HW.EForm.Core/Models/User.cs
+++ src/HW.EForm.Core/Models/User.cs
@@ -1,11 +1,4 @@
-﻿/*
- * Created by SharpDevelop.
- * User: Ian
- * Date: 7/13/2016
- * Time: 6:52 PM
- * 
- * To change this template use Tools | Options | Coding | Edit Standard Headers.
- */
+﻿
 using System;
 using HW.Core.Models;
 
