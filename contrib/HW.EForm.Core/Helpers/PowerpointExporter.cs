283b3325032a4825f05df90e9eed5ddf138a1717
iescarro <ian.escarro@gmail.com>
2016-08-24 17:44:32 +0800

    updated templates

---
+++ contrib/HW.EForm.Core/Helpers/PowerpointExporter.cs
@@ -0,0 +1,16 @@
+﻿// <file>
+//  <license></license>
+//  <owner name="Ian Escarro" email="ian.escarro@gmail.com"/>
+// </file>
+
+using System;
+
+namespace HW.EForm.Core.Helpers
+{
+	public class PowerpointExporter
+	{
+		public PowerpointExporter()
+		{
+		}
+	}
+}
