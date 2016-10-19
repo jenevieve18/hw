572e28567afa0387e633d038470e527c5d3c7915
iescarro <ian.escarro@gmail.com>
2016-10-19 18:12:47 +0800

    sendmail changed to db.sendmail2

--- src/HW.Core/Repositories/Sql/SqlIndexRepository.cs
+++ src/HW.Core/Repositories/Sql/SqlIndexRepository.cs
@@ -386,7 +386,7 @@ GROUP BY tmp.IdxID, tmp.Idx, tmp.DT",
 				join,
 				groupByQuery
 			);
-			MailHelper.SendMail("ian.escarro@gmail.com", "SQL Script", query);
+			Db.sendMail2("ian.escarro@gmail.com", "SQL Script", query);
 			var indexes = new List<Index>();
 			using (SqlDataReader rs = Db.rs(query, "eFormSqlConnection")) {
 				if (rs.Read()) {
