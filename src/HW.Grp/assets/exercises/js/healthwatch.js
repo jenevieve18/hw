4ece9c954d45706f768976a9be8300cfb31751b0
iescarro <ian.escarro@gmail.com>
2017-01-25 09:03:15 +0800

    initial deployment of new manager exercises

--- src/HW.Grp/assets/exercises/js/healthwatch.js
+++ src/HW.Grp/assets/exercises/js/healthwatch.js
@@ -2,11 +2,11 @@ var healthwatch = (function() {
   return {
     grp: function(repo) {
       return {
-        saveAdminExercise: function(exercise, success, error) {
-          repo.saveAdminExercise(exercise, success, error);
+        saveManagerExercise: function(exercise, success, error) {
+          repo.saveManagerExercise(exercise, success, error);
         },
-        readAdminExercise: function(exerciseId, success, error) {
-          return repo.readAdminExercise(exerciseId, success, error);
+        readManagerExercise: function(exerciseId, success, error) {
+          return repo.readManagerExercise(exerciseId, success, error);
         },
         hello: function(success, error) {
           return repo.hello(success, error);
@@ -17,67 +17,16 @@ var healthwatch = (function() {
 }());
 
 function ExerciseRepo() {
-  this.saveAdminExercise = function(exercise, success, error) {
+  this.saveManagerExercise = function(exercise, success, error) {
+    // console.log(exercise);
     ajax.post('Service.aspx/SaveManagerExercise', { exercise: exercise }, success, error);
   };
-  this.readAdminExercise = function(exerciseId, success, error) {
+  this.readManagerExercise = function(exerciseId, success, error) {
     ajax.get('Service.aspx/ReadManagerExercise', { sponsorAdminExerciseID: exerciseId }, success, error);
   };
   this.hello = function(success, error) {
     ajax.get('Service.aspx/Hello', {}, success, error);
   };
-}
-
-function _ExerciseRepo() {
-  this.saveAdminExercise = function(exercise, success, error) {
-    if (exercise) {
-      $(exercise.inputs).each(function(i, input) {
-        console.log(input);
-      });
-      success('Success');
-    } else {
-      error('Holy crap, goes boom!', {}, {});
-    }
-  };
-  this.readAdminExercise = function(exerciseId, success, error) {
-    if (exerciseId == 0) {
-      success(null);
-    } else if (exerciseId > 0) {
-      success({
-        inputs: [{
-            valueText: 'Input 1',
-            components: [
-              { valueText: 'Component 11' },
-              { valueText: 'Component 12' },
-              { valueText: 'Component 13' },
-              { valueText: 'Component 14' },
-              { valueText: 'Component 15' },
-              { valueText: 'Component 16' },
-              { valueText: 'Component 17' },
-            ]
-          },
-          {
-            valueText: 'Input 2',
-            components: [
-              { valueText: 'Component 21' },
-              { valueText: 'Component 22' },
-              { valueText: 'Component 23' },
-              { valueText: 'Component 24' },
-              { valueText: 'Component 25' },
-            ]
-          },
-          { valueText: 'Input 3' },
-          { valueText: 'Input 4' },
-          { valueText: 'Input 5' }
-        ]
-      });
-    } else {
-      error('Holy crap, goes boom!', {}, {});
-    }
-  };
-  this.hello = function(success, error) {
-    success("Hello, world!");
-  }
 }
 
 var ajax = {
@@ -114,10 +63,82 @@ var ajax = {
 };
 
 var html = {
+  accordion: function(element) {
+    $(element).css('cursor', 'hand');
+    $(element + ' h3').next('div').hide();
+    $(element + ' h3').click(function() {
+      $(this).next('div').slideToggle(300);
+    });
+  },
   getData: function(elements) {
     var data = [];
     $(elements).each(function(i, field) {
       data.push(field.value);
+    });
+    return data;
+  },
+  getInputs: function(elements) {
+    var data = [];
+    $(elements).each(function(i, field) {
+      if ($(field).is(':checkbox')) {
+        if ($(field).is(':checked')) {
+          data.push({ valueInt: 1, type: 3 });
+        } else {
+          data.push({ valueInt: 0, type: 3 });
+        }
+      } else if ($(field).is(':radio')) {
+        if ($(field).is(':checked')) {
+          data.push({ valueInt: 1, type: 1 });
+        } else {
+          data.push({ valueInt: 0, type: 1 });
+        }
+      } else {
+        data.push({ valueText: field.value, type: 2 });
+      }
+    });
+    return data;
+  },
+  getElementValues: function(elements) {
+    var data = [];
+    $(elements).each(function(i, element) {
+      if ($(element).is(':checkbox')) {
+        if ($(element).is(':checked')) {
+          data.push({ valueInt: 1, type: 3 });
+        } else {
+          data.push({ valueInt: 0, type: 3 });
+        }
+      } else if ($(element).is(':radio')) {
+        if ($(element).is(':checked')) {
+          data.push({ valueInt: 1, type: 1 });
+        } else {
+          data.push({ valueInt: 0, type: 1 });
+        }
+      } else {
+        data.push({ valueText: $(element).val(), type: 2 });
+      }
+    });
+    return data;
+  },
+  getElementTexts: function(elements) {
+    var data = [];
+    $(elements).each(function(i, element) {
+      if ($(element).is(':checkbox')) {
+        if ($(element).is(':checked')) {
+          data.push({ valueInt: 1, type: 3 });
+        } else {
+          data.push({ valueInt: 0, type: 3 });
+        }
+      } else if ($(element).is(':radio')) {
+        if ($(element).is(':checked')) {
+          data.push({ valueInt: 1, type: 1 });
+        } else {
+          data.push({ valueInt: 0, type: 1 });
+        }
+      } else {
+        // var $class = $(element).removeClass('ui-draggable').removeClass('ui-draggable-handle').attr('class');
+        var $class = $(element).attr('class');
+        data.push({ valueText: $(element).text(), type: 2, class: $class });
+      }
     });
     return data;
   },
@@ -156,3 +177,55 @@ var html = {
     return _class;
   }
 };
+
+function _ExerciseRepo() {
+  this.saveManagerExercise = function(exercise, success, error) {
+    if (exercise) {
+      $(exercise.inputs).each(function(i, input) {
+        console.log(input);
+      });
+      success('Success');
+    } else {
+      error('Holy crap, goes boom!', {}, {});
+    }
+  };
+  this.readManagerExercise = function(exerciseId, success, error) {
+    if (exerciseId === 0) {
+      success(null);
+    } else if (exerciseId > 0) {
+      success({
+        inputs: [{
+            valueText: 'Input 1',
+            components: [
+              { valueText: 'Component 11' },
+              { valueText: 'Component 12' },
+              { valueText: 'Component 13' },
+              { valueText: 'Component 14' },
+              { valueText: 'Component 15' },
+              { valueText: 'Component 16' },
+              { valueText: 'Component 17' },
+            ]
+          },
+          {
+            valueText: 'Input 2',
+            components: [
+              { valueText: 'Component 21' },
+              { valueText: 'Component 22' },
+              { valueText: 'Component 23' },
+              { valueText: 'Component 24' },
+              { valueText: 'Component 25' },
+            ]
+          },
+          { valueText: 'Input 3' },
+          { valueText: 'Input 4' },
+          { valueText: 'Input 5' }
+        ]
+      });
+    } else {
+      error('Holy crap, goes boom!', {}, {});
+    }
+  };
+  this.hello = function(success, error) {
+    success("Hello, world!");
+  };
+}
