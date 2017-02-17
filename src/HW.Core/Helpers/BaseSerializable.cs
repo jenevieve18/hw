eeab0685d631da5d1dbcbb43de0a4b74e871cf7b
iescarro <ian.escarro@gmail.com>
2017-02-17 08:55:16 +0800

    added base serializable for xml serialization

---
+++ src/HW.Core/Helpers/BaseSerializable.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace HW.Core.Helpers
+{
+	public class BaseSerializable<T>
+	{
+		public BaseSerializable()
+		{
+		}
+		
+		public static T Deserialize(string filename)
+		{
+			return Deserialize(new StreamReader(filename));
+		}
+		
+		public static T Deserialize(TextReader reader)
+		{
+			XmlSerializer serializer = new XmlSerializer(typeof(T));
+			T type = (T)serializer.Deserialize(reader);
+			reader.Close();
+			return type;
+		}
+	}
+}
