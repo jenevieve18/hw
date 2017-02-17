084b03d29c93de789a88d6a67dc03239fe5bbaeb
Ian Escarro <ian.escarro@gmail.com>
2017-02-17 14:08:06 +0800

    updated sql schema for user registration id

--- doc/20170209_UserRegistrationID.sql
+++ doc/20170209_UserRegistrationID.sql
@@ -1,30 +1,15 @@
-USE [healthwatch]
-GO
+use healthwatch
+go
 
-/****** Object:  Table [dbo].[UserRegistrationID]    Script Date: 02/09/2017 13:48:12 ******/
-SET ANSI_NULLS ON
-GO
+create table UserRegistrationID(
+	UserRegistrationID integer not null primary key identity,
+	UserID integer,
+	RegistrationID varchar(255)
+)
+go
 
-SET QUOTED_IDENTIFIER ON
-GO
+alter table UserRegistrationID add PhoneName varchar(255)
+go
 
-SET ANSI_PADDING ON
-GO
-
-CREATE TABLE [dbo].[UserRegistrationID](
-	[UserRegistrationID] [int] IDENTITY(1,1) NOT NULL,
-	[UserID] [int] NULL,
-	[RegistrationID] [varchar](255) NULL,
-	[PhoneName] [varchar](255) NULL,
-	[Inactive] [int] NULL,
-PRIMARY KEY CLUSTERED 
-(
-	[UserRegistrationID] ASC
-)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
-) ON [PRIMARY]
-
-GO
-
-SET ANSI_PADDING OFF
-GO
-
+alter table UserRegistrationID add Inactive integer
+go
