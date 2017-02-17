0c187eee101146452cabe9ee93bef39fef575a8c
Ian Escarro <ian.escarro@gmail.com>
2017-02-17 14:23:11 +0800

    separated the 2 scripts for create table and alter with phone and inactive columns

--- doc/20170209_UserRegistrationID.sql
+++ doc/20170209_UserRegistrationID.sql
@@ -7,9 +7,3 @@ create table UserRegistrationID(
 	RegistrationID varchar(255)
 )
 go
-
-alter table UserRegistrationID add PhoneName varchar(255)
-go
-
-alter table UserRegistrationID add Inactive integer
-go
---
+++ doc/20170209_UserRegistrationID_PhoneInactive.sql
@@ -0,0 +1,8 @@
+use healthwatch
+go
+
+alter table UserRegistrationID add PhoneName varchar(255)
+go
+
+alter table UserRegistrationID add Inactive integer
+go
