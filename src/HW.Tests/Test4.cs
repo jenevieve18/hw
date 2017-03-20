56beb88ea389b5a9f02c4d8917b22fe6c52924fc
Ian Escarro <ian.escarro@gmail.com>
2017-03-20 19:58:36 +0800

    more tests

---
+++ src/HW.Tests/Test4.cs
@@ -0,0 +1,17 @@
+﻿
+using System;
+using System.Security.Cryptography;
+using NUnit.Framework;
+
+namespace HW.Tests
+{
+	[TestFixture]
+	public class Test4
+	{
+		[Test]
+		public void TestMethod()
+		{
+			
+        }
+	}
+}
