51f4db3153f3cd35a05d85c8a4f32ed763d100ef
Ian Escarro <ian.escarro@gmail.com>
2017-03-20 19:57:46 +0800

    new test

---
+++ src/HW.Grp.Tests/Util/Test1.cs
@@ -0,0 +1,18 @@
+﻿
+using System;
+using System.Security.Cryptography;
+using System.Text;
+using NUnit.Framework;
+
+namespace HW.Grp.Tests.Util
+{
+	[TestFixture]
+	public class Test1
+	{
+		[Test]
+		public void TestMethod()
+		{
+			Console.WriteLine(Guid.NewGuid().ToString().Replace("-", ""));
+		}
+	}
+}
