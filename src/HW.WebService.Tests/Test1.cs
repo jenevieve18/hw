6c317bb7d8787d7c4308eb7bc2b5c2c673b3447f
Ian Escarro <ian.escarro@gmail.com>
2017-03-20 19:57:31 +0800

    added web service test

---
+++ src/HW.WebService.Tests/Test1.cs
@@ -0,0 +1,44 @@
+﻿
+using System;
+using NUnit.Framework;
+
+namespace HW.WebService.Tests
+{
+	[TestFixture]
+	public class Test1
+	{
+		Service s;
+		
+		[SetUpAttribute]
+		public void Setup()
+		{
+			s = new Service(new DummyRequest());
+		}
+		
+		class DummyRequest : Service.IRequest
+		{
+			public string UserHostAddress {
+				get { return "test"; }
+			}
+		}
+		
+		[Test]
+		public void TestMethod()
+		{
+			var u = s.UserLogin("test1", "password", 10);
+			
+			var token = u.token;
+			
+			Assert.IsTrue(s.UserDisable2FA(token, 10));
+			
+			Assert.IsTrue(s.UserEnable2FA(token, 10));
+		}
+		
+		[Test]
+		public void a()
+		{
+			var u = s.UserLogin("test1", "password", 10);
+			Console.WriteLine(s.UserGenerateSecret(u.token, 10));
+		}
+	}
+}
