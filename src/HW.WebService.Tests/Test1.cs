6c2a0bcfe696c092cfed81db8943cf0449543e9d
iescarro <ian.escarro@gmail.com>
2017-03-27 14:57:46 +0800

    fixed the unblocked

---
+++ src/HW.WebService.Tests/Test1.cs
@@ -0,0 +1,17 @@
+﻿
+using System;
+using NUnit.Framework;
+
+namespace HW.WebService.Tests
+{
+	[TestFixture]
+	public class Test1
+	{
+		[Test]
+		public void TestMethod()
+		{
+//			var s = new Service();
+//			s.UserCreate("test1", "password", "test1@localhost.com", "", true, 2, 0, 0, 10);
+		}
+	}
+}
