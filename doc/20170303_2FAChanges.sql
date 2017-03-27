c089971e55c9109da4d9053353680c52fced29db
Ian Escarro <ian.escarro@gmail.com>
2017-03-27 14:01:32 +0800

    changed the 2fa tables

--- doc/20170303_2FAChanges.sql
+++ doc/20170303_2FAChanges.sql
@@ -4,13 +4,21 @@ alter table [User] add Enable2FA integer;
 
 alter table Sponsor add Enable2FA integer;
 
+--drop table UserLogin;
 create table UserLogin(
-	UserLoginAttemptID integer not null primary key identity,
+	UserLoginID integer not null primary key identity,
 	UserID integer,
 	IPAddress varchar(255),
 	LoginAttempt datetime,
 	ResourceID varchar(255),
-	SecretKey varchar(255)
+	UserToken varchar(255),
+	ActiveLoginAttempt integer,
+	Unblocked integer
 );
 
-alter table UserLogin add UserToken varchar(255);
+--drop table UserSecret;
+create table UserSecret(
+	UserSecretID integer not null primary key identity,
+	UserID integer,
+	SecretKey varchar(255)
+);
