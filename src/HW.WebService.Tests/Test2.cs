1d3df158e3ca59683176345d027829cbd8088048
Ian Escarro <ian.escarro@gmail.com>
2017-04-10 20:47:59 +0800

    added new test cases

---
+++ src/HW.WebService.Tests/Test2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace HW.WebService.Tests
+{
+	[TestFixture]
+	public class Test2
+	{
+		[Test]
+		public void TestMethod()
+		{
+			var s = new Service(new DummyRequest());
+			var u = s.UserLogin("test1", "password", 10);
+			var f = s.FormEnum(u.token, 2, 10);
+			s.UserGetFormInstanceFeedback(u.token, f[0].formKey, "", 2, 10);
+		}
+	}
+}
