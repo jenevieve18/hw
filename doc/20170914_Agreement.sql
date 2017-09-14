670cdf3f480da92757e868584ae1dda3176ef6de
Ian Escarro <ian.escarro@gmail.com>
2017-09-14 17:47:54 +0800

    Added schema for agreement.

---
+++ doc/20170914_Agreement.sql
@@ -0,0 +1,36 @@
+use healthwatch;
+
+create table UserAgreement(
+	UserAgreementID integer not null primary key identity,
+	UserID integer,
+	AgreementID integer,
+	AgreementDate datetime
+);
+
+create table Agreement(
+	AgreementID integer not null primary key identity,
+	EffectivityDate datetime,
+	Name varchar(255),
+	AgreementContentID integer,
+	Type integer
+);
+
+create table AgreementLang(
+	AgreementLangID integer not null primary key identity,
+	AgreementID integer,
+	Name varchar(255),
+	LangID integer
+);
+
+create table AgreementContent(
+	AgreementContentID integer not null primary key identity,
+	Name varchar(255)
+);
+
+create table AgreemeentContentLang(
+	AgreementContentLangID integer not null primary key identity,
+	AgreementText text,
+	Name varchar(255),
+	AgreementContentID integer,
+	LangID integer
+);
